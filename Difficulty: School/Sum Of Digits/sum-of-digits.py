9c09551d26ef1e9a4b7086d7c37084e926b21d50
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-18 19:56:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Sum Of Digits/sum-of-digits.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def sumOfDigits (self, N):
+        # code here
+        sum = 0
+        while N > 0:
+            sum += N % 10
+            N = N // 10
+        return sum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+       
+
+        ob = Solution()
+        print(ob.sumOfDigits(N))
+# } Driver Code Ends
